12bcbc90db89333d1be94e4993f6d9ba485bfed5
Rachel Kurchin <rkurchin@cmu.edu>
2022-01-06 09:48:18 -0600

    copy over basic functionality
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- src/AtomGraphs.jl
+++ src/AtomGraphs.jl
@@ -1,5 +1,21 @@
 module AtomGraphs
 
-# Write your package code here.
+using Graphs
+using SimpleWeightedGraphs
+using LinearAlgebra
+using GraphPlot
+using Colors
+using PyCall
+using Serialization
+using Xtals
+#rc[:paths][:crystals] = @__DIR__ # so that Xtals.jl knows where things are
+using MolecularGraph
+
+include("atomgraph.jl")
+export AtomGraph
+
+include("graph_building.jl")
+export build_graph
+export inverse_square, exp_decay
 
 end
